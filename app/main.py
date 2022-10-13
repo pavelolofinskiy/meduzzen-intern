230b79e6b1b8b088e2102df73a5c561e8eb9a1aa
PavelOlofinskiy <pavelolofinskiy@gmail.com>
2022-10-13 11:38:21 +0300

    add docker-compose file and first ver of db connect in main.py

--- app/main.py
+++ app/main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
+import databases
+
+
+DATABASE_URL = 'postgresql://postgres:postgres@127.0.0.1:5432/default_database'
+
+postgres_db = databases.Database(DATABASE_URL)
 
 app = FastAPI()
 
 
-@app.get("/")
-async def health_check():
-    return {"status": "Working"}
+@app.on_event("startup")
+async def startup():
+    await postgres_db.connect()
+
+
+
+
