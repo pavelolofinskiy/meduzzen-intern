065df0f145f05392fd02453a1d1fbf6ba21e2a50
PavelOlofinskiy <pavelolofinskiy@gmail.com>
2022-10-14 12:48:59 +0300

    add db connect

--- app/main.py
+++ app/main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
+from db import database_pg
+from db import database_rd
+
+url_redis = 'redis://:password@hostname:port/0'
+
 
 app = FastAPI()
 
 
-@app.get("/")
-async def health_check():
-    return {"status": "Working"}
+@app.on_event("startup")
+async def startup():
+    await database_pg.connect()
+
+
+@app.on_event("startup")
+async def startup():
+    await database_rd.connect()
