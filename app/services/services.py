3ea23f79e3cf204265e9f2505aa5372ed9294714
PavelOlofinskiy <pavelolofinskiy@gmail.com>
2022-10-28 10:35:02 +0300

    fix crud

--- app/services/services.py
+++ app/services/services.py
@@ -1,39 +1,36 @@
-from app.database.db import postgres_db, Session
-from app.shemas.schemas import UserUpdate, UserCreate, ResponseUserId
+from app.database.db import postgres_db
+from app.shemas.schemas import UserUpdate, UserCreate, ResponseUserId, PublicUser, Users
 from app.models.models import users as DBUser
 from fastapi import HTTPException, status
 
 
 class UserCrud:
-    @staticmethod
-    async def create_user(user: UserCreate) -> ResponseUserId:
-        db_user = DBUser.insert().values(email=user.email, username=user.username, password=user.password)
+    def __init__(self, db_user: DBUser = None):
+        self.db_user = db_user
+
+    async def create_user(self, user: UserCreate) -> ResponseUserId:
+        db_user = self.db_user.insert().values(email=user.email, username=user.username, password=user.password)
         user_id = await postgres_db.execute(db_user)
         if user_id:
-            raise HTTPException(403, detail=f'User {user_id} already exist')
+            raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail=f'user with id {user_id} already exists')
         return ResponseUserId(id=user_id)
 
-    @staticmethod
-    async def get_users():
-        results = await postgres_db.fetch_all(DBUser.select())
-        return [dict(result) for result in results]
+    async def get_users(self) -> Users:
+        results = await postgres_db.fetch_all(self.db_user.select())
+        users = [dict(result) for result in results]
+        return Users(users=users)
 
-    @staticmethod
-    async def get_by_id(id: int) -> UserCreate:
-        user = postgres_db.fetch_one(DBUser.select().where(DBUser.c.id == id))
-        return UserCreate(**user)
+    async def get_by_id(self, id: int) -> PublicUser:
+        user = await postgres_db.fetch_one(self.db_user.select().where(self.db_user.c.id == id))
+        return PublicUser(**user)
 
-    @staticmethod
-    async def update_user(id: int, user_in: UserUpdate) -> ResponseUserId:
+    async def update_user(self, id: int, user_in: UserUpdate) -> ResponseUserId:
         updated_data = user_in.dict()
-        update = DBUser.update().values(updated_data).where(DBUser.c.id == id)
+        update = self.db_user.update().values(updated_data).where(self.db_user.c.id == id)
         await postgres_db.execute(update)
         return ResponseUserId(id=id)
 
-    @staticmethod
-    async def delete(id: int) -> ResponseUserId:
-        delete = DBUser.delete().where(DBUser.c.id == id)
+    async def delete_user(self, id: int) -> ResponseUserId:
+        delete = self.db_user.delete().where(self.db_user.c.id == id)
         await postgres_db.execute(delete)
         return ResponseUserId(id=id)
-
-
